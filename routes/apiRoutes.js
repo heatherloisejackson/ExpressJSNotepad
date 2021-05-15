7298b44870d14d4d2ebd7f2e91f9d43d1ec88c9b
Heather Jackson <hmjackson917@gmail.com>
2021-05-15 13:22:55 -0400

    comment adjustments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,19 @@
-// The following API routes should be created:
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
-// and then return the new note to the client. You'll need to find a way to give each note a unique
-// id when it's saved (look into npm packages that could do this for you).
-
 const fs = require("fs");
 const uniqid = require("uniqid");
 const noteData = require("../db/db.json");
 
 module.exports = (app) => {
+  // read the db.json file and return all saved notes as JSON
   app.get("/api/notes", (req, res) => res.json(noteData));
 
   app.post("/api/notes", (req, res) => {
     const newNote = req.body;
+    // create a unique ID for each note using the uniqid library
     newNote.id = uniqid();
 
     noteData.push(newNote);
 
+    // convert user's note into JSON and update db.json file
     fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) =>
       err ? console.error(err) : res.json(newNote)
     );
